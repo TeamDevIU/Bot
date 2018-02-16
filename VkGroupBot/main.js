ad5abdf73cdcbc9dbebf2b0129941257b2f6fb0f
pacman29 <zx19-95@mail.ru>
2018-02-17 01:49:53 +0300

    TeamDevIU/Bot#5  TeamDevIU/Bot#14

--- VkGroupBot/main.js
+++ VkGroupBot/main.js
@@ -1,11 +1,11 @@
 const { Bot } = require('node-vk-bot');
 
 let bot = new Bot({
-    token: '7335acdfd581500a090c90407434802553a0bc61c49966fe674a28892a29175aeed6a4580a01b29268301'
+    token: '<token>'
 }).start();
 
 let apiai = require('apiai');
-let dialogflow = apiai("ffe0e0524c5f4c6eab9ea38d38d5f2f6");
+let dialogflow = apiai("<client_token>");
 
 bot.get(/.*/i, msg => {
     console.log(msg);
