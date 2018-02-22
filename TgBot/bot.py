cb9f4f08774f7e0c7e2bbb8c87205f864de6656a
Dimama <citrus_m@mail.ru>
2018-02-22 08:52:36 +0300

    TeamDevIU/Bot#27 Добавлены строки документирования

--- TgBot/bot.py
+++ TgBot/bot.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""Основной модуль приложения, содержащий функции-обработчики пользовательских сообщений
+    Используется веб-сервер библиотеки aiohttp для запуска бота
+    с использованием webhook
+"""
 
-from aiohttp import web
+import json
+import requests
 import telebot
+from aiohttp import web
 from config import API_TOKEN
 from webhookConfig import *
 
@@ -13,8 +19,13 @@ bot = telebot.TeleBot(API_TOKEN)
 
 app = web.Application()
 
-# Process webhook calls
 async def handle(request):
+    """ Функция-обработчик запросов, поступающих на веб-сервер
+
+    :param request: объект-запрос
+
+    """
+
     if request.match_info.get('token') == bot.token:
         request_body_dict = await request.json()
         update = telebot.types.Update.de_json(request_body_dict)
@@ -25,14 +36,32 @@ async def handle(request):
 
 app.router.add_post('/{token}/', handle)
 
+@bot.message_handler(content_types=['voice'])
+def handleAudio(message):
+    """ Обработчик голосовых сообщений пользователя
+    
+    :param message: сообщение пользователя
+
+    """
+    fileInfo = bot.get_file(message.voice.file_id)
+    bot.send_message(message.chat.id, "я не понимаю аудиосообщения :(")
+    file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.
+                        format(API_TOKEN, fileInfo.file_path))
+                        
+
 @bot.message_handler()
-def echo_message(message):
+def echoMessage(message):
+    """ Обработчик всех сообщений пользователя
+    
+    :param message: сообщение пользователя
+
+    """
     bot.reply_to(message, message.text)
+
 
 bot.remove_webhook()
 bot.set_webhook(url=WEBHOOK_URL_BASE+WEBHOOK_URL_PATH)
 
-# Start aiohttp server
 web.run_app(
     app,
     host=WEBHOOK_LISTEN,
