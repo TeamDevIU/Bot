50194a699c8439b0706ce33e0322d2782807cedc
pacman29 <zx19-95@mail.ru>
2018-02-26 20:11:41 +0300

    TeamDev/Bot#19

---
+++ .travis/push_to_master.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  git checkout -b gh-pages
+  git add . 
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add origin-pages https://${GITHAB_TOKEN}@github.com/TeamDevIU/Bot.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin-master 
+}
+
+setup_git
+commit_website_files
+upload_files
