1499768abc2bf96b0259541fa1440f6dbba9d4ce
Pacman29 <zx19-95@mail.ru>
2018-03-04 02:59:28 +0300

    Update deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -10,27 +10,27 @@ ssh -o "StrictHostKeyChecking no" botuser@$PUBLIC_HOST <<EOF
   cd Bot;
   git pull;
   rm TgBot/config.py;
-  echo "SERVER_HOST=\"http://$PRIVATE_HOST:8080\"\n /
-            WEBHOOK_HOST=\"$SERVER_DNS/tg\"\n /
-            API_TOKEN=\"$API_TOKEN\"\n /
-            DIALOG_FLOW_TOKEN=\"$DIALOG_FLOW_TOKEN\"\n /
-            WEBHOOK_LISTEN=\"0.0.0.0\"\n  /
-            WEBHOOK_PORT=\"8002\"\n /
+  echo "SERVER_HOST=\"http://$PRIVATE_HOST:8080\"
+            WEBHOOK_HOST=\"$SERVER_DNS/tg\"
+            API_TOKEN=\"$API_TOKEN\"
+            DIALOG_FLOW_TOKEN=\"$DIALOG_FLOW_TOKEN\"
+            WEBHOOK_LISTEN=\"0.0.0.0\"
+            WEBHOOK_PORT=\"8002\"
             YANDEX_SPEECH_TOKEN=\"$YANDEX_SPEECH_TOKEN\"" > TgBot/config.py;
   cat TgBot/config.py;         
   rm VkGroupBot/config.json;
-  echo "{\"YANDEX_DEV_KEY\":\"$YANDEX_SPEECH_TOKEN\",\n /
-             \"VKTOKEN\":\"$VK_TOKEN\",\n /
-             \"DIALOGFLOW_CLIENT_ID\":\"$DIALOG_FLOW_TOKEN\",\n /
-             \"PORT_BOT\":8000 \n, /
-             \"URL\":\"https://$PUBLIC_HOST/vk/callback\",\n /
-             \"MAINSERVER_URL\":\"$PRIVATE_HOST:8080\",\n /
+  echo "{\"YANDEX_DEV_KEY\":\"$YANDEX_SPEECH_TOKEN\",
+             \"VKTOKEN\":\"$VK_TOKEN\",
+             \"DIALOGFLOW_CLIENT_ID\":\"$DIALOG_FLOW_TOKEN\",
+             \"PORT_BOT\":8000,
+             \"URL\":\"https://$PUBLIC_HOST/vk/callback\",
+             \"MAINSERVER_URL\":\"$PRIVATE_HOST:8080\",
              \"WORKER\":2}" > VkGroupBot/config.json;
  cat VkGroupBot/config.json;
  rm main-server/run.sh;
- echo "go get github.com/gorilla/mux\n/
-       go get github.com/lib/pq\n/
-       go build\n/
+ echo "go get github.com/gorilla/mux
+       go get github.com/lib/pq
+       go build
        ./go -dbuser=$PG_USER -dbpass=$PG_PASSWORD -dbname=botdb -tgbot=$PRIVATE_HOST:8002 -vkbot=$PUBLIC_HOST:8000 -port=8080" > main-server/run.sh;
  cat main-server/run.sh;
  sudo docker-compose build --no-cache;
