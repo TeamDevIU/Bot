029d30e524051f119e3ca91c538c899e6a471c84
Pacman29 <zx19-95@mail.ru>
2018-03-05 02:16:08 +0300

    bug fix

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -32,7 +32,7 @@ YANDEX_SPEECH_TOKEN=\"$YANDEX_SPEECH_TOKEN\"" > TgBot/config.py;
  echo "go get github.com/gorilla/mux
        go get github.com/lib/pq
        go build
-       ./go -dbhost=10.0.0.4 -dbport=5432 -dbuser=$PG_USER -dbpass=$PG_PASSWORD -dbname=botdb -tgbot=$PRIVATE_HOST:8002 -vkbot=$PRIVATE_HOST:8000/send_message -port=8080" > main-server/run.sh;
+       ./go -dbhost=10.0.0.4 -dbport=5432 -dbuser=$PG_USER -dbpass=$PG_PASSWORD -dbname=botdb -tgbot=http://$PRIVATE_HOST:8002 -vkbot=http://$PRIVATE_HOST:8000/send_message -port=8080" > main-server/run.sh;
  cat main-server/run.sh;
  sudo docker-compose build --no-cache;
  sudo docker-compose kill;
