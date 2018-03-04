96d29200316789e4b354f020a3fcaf377e214c88
Pacman29 <zx19-95@mail.ru>
2018-03-05 01:41:28 +0300

    Update deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -35,5 +35,6 @@ YANDEX_SPEECH_TOKEN=\"$YANDEX_SPEECH_TOKEN\"" > TgBot/config.py;
        ./go -dbhost=10.0.0.4 -dbport=5432 -dbuser=$PG_USER -dbpass=$PG_PASSWORD -dbname=botdb -tgbot=$PRIVATE_HOST:8002 -vkbot=$PRIVATE_HOST:8000/send_message -port=8080" > main-server/run.sh;
  cat main-server/run.sh;
  sudo docker-compose build --no-cache;
- sudo docker-compose up -d
+ sudo docker-compose kill;
+ sudo docker-compose up -d;
 EOF
